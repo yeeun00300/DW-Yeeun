eedefbea5790a69682dc78473e8dfa3013ae07ea
yeeun <mlikshoot99@gmail.com>
2024-07-01 17:36:43 +0900

    /

--- REACT/02-rsp-game/src/App.js
+++ REACT/02-rsp-game/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
-
+import reset from "./assets/ic-reset.svg";
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <div className="header">
+        <h3 className="title">가위바위보</h3>
+        <img src={reset} alt="" className="reset" />
+      </div>
+      <div className="scoreBoard">
+        <div className="score me">
+          <div className="number">0</div>
+          <div className="text">나</div>
+        </div>
+        <div>:</div>
+        <div className="score you">
+          <div className="number">0</div>
+          <div className="text">상대</div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
