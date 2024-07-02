f01d75be9acead4a322b0d2b3f34c10f0c582fb6
chajoonho <mlikshoot99@gmail.com>
2024-07-02 15:04:56 +0900

    /

--- REACT/02-rsp-game/src/App.js
+++ REACT/02-rsp-game/src/App.js
@@ -4,11 +4,23 @@ import HandIcon from "./handIcon";
 import "./handIcon.css";
 import HandButton from "./HandButton";
 import { useState } from "react";
-import { generateRandomHand } from "./utils";
+import { compareHand, generateRandomHand } from "./utils";
+
+function getResult(comparison) {
+  if (comparison > 0) return "승리";
+  if (comparison < 0) return "패배";
+  return "무승부";
+}
 
 function App() {
   const [hand, setHand] = useState("rock");
   const [otherHand, setOtherHand] = useState("rock");
+  const [score, setScore] = useState(0);
+  const [otherScore, setOtherScore] = useState(0);
+  const [bet, setBet] = useState(1);
+  const [gameHistory, setGameHistory] = useState([]);
+  const [isWin, setIsWin] = useState(0);
+
   const handleButtonClick = (value) => {
     // 사용자가 클릭한 주먹가위보 가져오기
     setHand(value);
@@ -16,43 +28,84 @@ function App() {
     const nextOtherHand = generateRandomHand();
     setOtherHand(nextOtherHand);
     // 승패 결정 ==> 배점을 곱해서 점수 추출
+    const comparison = compareHand(value, nextOtherHand);
+    setIsWin(comparison);
+    if (comparison > 0) setScore(score + bet);
+    if (comparison < 0) setOtherScore(otherScore + bet);
+    // 결정된 승패를 state에 저장
+    const result = getResult(comparison);
+    setGameHistory([...gameHistory, result]);
   };
+
+  const handleBetChange = (e) => {
+    let num = Number(e.target.value);
+    if (num > 9) num %= 10;
+    if (num < 1) num = 1;
+    num = Math.floor(num);
+    setBet(num);
+  };
+
+  const handleClearClick = () => {
+    setHand("rock");
+    setOtherHand("rock");
+    setScore(0);
+    setOtherScore(0);
+    setBet(1);
+    setGameHistory([]);
+    setIsWin(0);
+  };
+
   return (
     <div className="App">
       <h1 className="App-heading">가위바위보</h1>
-      <img src={reset} alt="" className="App-reset" />
+      <img
+        src={reset}
+        alt=""
+        className="App-reset"
+        onClick={handleClearClick}
+      />
       <div className="App-scores">
         <div className="Score">
-          <div className="Score-num">0</div>
+          <div className="Score-num">{score}</div>
           <div className="Score-name">나</div>
         </div>
         <div className="App-versus">:</div>
         <div className="Score">
-          <div className="Score-num">0</div>
+          <div className="Score-num">{otherScore}</div>
           <div className="Score-name">상대</div>
         </div>
       </div>
       <div className="Box App-box">
         {/* 가위바위보 내는곳 */}
         <div className="App-hands">
-          <div className="Hand">
+          <div
+            className={`Hand ${isWin == 0 ? "" : isWin == 1 ? "winner" : ""}`}
+          >
             <HandIcon value={hand} className="Hand-icon" />
           </div>
           <div className="App-versus">VS</div>
-          <div className="Hand">
+          <div
+            className={`Hand ${isWin == 0 ? "" : isWin == 1 ? "" : "winner"}`}
+          >
             <HandIcon value={otherHand} className="Hand-icon" />
           </div>
         </div>
         {/* 배점 */}
         <div className="App-bet">
           <span>배점</span>
-          <input type="number" min={1} max={9} />
+          <input
+            type="number"
+            min={1}
+            max={9}
+            value={bet}
+            onChange={handleBetChange}
+          />
           <span>배</span>
         </div>
         {/* 기록 */}
         <div className="App-history">
           <h2>승부기록</h2>
-          <p>승리, 패배, 무승부</p>
+          <p>{gameHistory.join(", ")}</p>
         </div>
       </div>
       <div>
--- REACT/02-rsp-game/src/utils.js
+++ REACT/02-rsp-game/src/utils.js
@@ -1,6 +1,20 @@
+const WINS = {
+  rock: "scissor",
+  scissor: "paper",
+  paper: "rock",
+};
+
 function random(n) {
   return Math.ceil(Math.random() * n);
 }
+
+export function compareHand(a, b) {
+  // 승리 ==> 1, 패배 => -1, 무승부 ==> 0
+  if (WINS[a] === b) return 1;
+  if (WINS[b] === a) return -1;
+  return 0;
+}
+
 export function generateRandomHand() {
   const num = random(3);
   if (num === 1) {
