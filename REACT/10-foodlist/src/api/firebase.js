3536dd76308add40eab6f1791e98a459403d344b
yeounghyeon <mlikshoot99@gmail.com>
2024-07-24 11:41:39 +0900

    /

---
+++ REACT/10-foodlist/src/api/firebase.js
@@ -0,0 +1,89 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  getDocs,
+  collection,
+  setDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+  getDoc,
+  updateDoc,
+  query,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import {
+  deleteObject,
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMFwBpirVXWXjth8zjOfB6cUjujTiIfiU",
+  authDomain: "foodlist-9d93e.firebaseapp.com",
+  projectId: "foodlist-9d93e",
+  storageBucket: "foodlist-9d93e.appspot.com",
+  messagingSenderId: "46801300187",
+  appId: "1:46801300187:web:ef353883eda4212c51bafe",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+function getCollection(collectionName) {
+  return collection(db, collectionName);
+}
+
+function createPath(path) {
+  const uuid = crypto.randomUUID();
+  return path + uuid;
+}
+
+async function addDatas(collectionName, addObj) {
+  // 파일 저장 ==> 스토리지의 이미지 url을 addObj의 imgUrl 값으로 변경
+  const path = createPath("food/");
+  const url = await uploadImage(path, addObj.imgUrl);
+  addObj.imgUrl = url;
+
+  // id 생성
+  const lastId = (await getLastNum(collectionName, "id")) + 1;
+  addObj.id = lastId;
+
+  // 시간 정보 생성
+  const time = new Date().getTime();
+  addObj.createdAt = time;
+  addObj.updatedAt = time;
+
+  // 컬렉션에 저장
+  await addDoc(getCollection(collectionName), addObj);
+}
+
+async function uploadImage(path, file) {
+  const storage = getStorage();
+  const imageRef = ref(storage, path);
+
+  // File 객체를 스토리지에 저장
+  await uploadBytes(imageRef, file);
+
+  // 저장한 File의 url을 받는다.
+  const url = await getDownloadURL(imageRef);
+  return url;
+}
+
+async function getLastNum(collectionName, field) {
+  const q = query(
+    getCollection(collectionName), // collection
+    orderBy(field, "desc"), // 정렬할 필드로 내림차순
+    limit(1) // 1개만 가져온다
+  );
+  const lastDoc = await getDocs(q);
+  const lastId = lastDoc.docs[0].data()[field];
+  return lastId;
+}
+
+export { addDatas };
